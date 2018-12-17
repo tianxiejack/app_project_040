3e2ba4837723b957c03ae6de04d104eb2995bc1a
huangqq <aloysa@163.com>
2018-12-17 20:51:36 +0800

    modify makefile lib

--- Release/objects.mk
+++ Release/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lopencv_highgui -lopencv_core -lopencv_video -lopencv_imgproc -lgomp -lGL -lGLEW -lglut -lcudart -lcrencOver422uart_040 -lcrosa_040 -lcrmtd_040 -lcrosd_040 -lcrgst_040 -lcrcore_040 -ldft_040 -lMMTD_040 -lenh_040 -lcrScene_040
+LIBS := -lopencv_highgui -lopencv_core -lopencv_video -lopencv_imgproc -lgomp -lGL -lGLEW -lglut -lcudart -lcrencOver422uart -lcrosa -lcrmtd -lcrosd -lcrgst -lcrcore -ldft -lMMTD -lenh -lcrScene
 
